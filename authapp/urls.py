718f4d71da6a3efdb5cafcdd43132fd3d7e6eae1
SHUBHAM GARG <sg8311@nyu.edu>
2024-06-24 14:18:15 -0400

    chnages

--- authapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from authapp import views
-
-urlpatterns = [
-    path('signup/',views.signup,name="signup"),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-]
